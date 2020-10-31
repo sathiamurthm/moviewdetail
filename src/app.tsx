366fd6bb3d4ae00c6523a57a1b25a8f56d591140
sathiamurthm <73694553+sathiamurthm@users.noreply.github.com>
2020-10-31 11:14:35 +0530

    Add files via upload

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,58 @@
-import * as React from "react";
-import { getNameOfDeclaration } from "typescript";
-import { HelloComponent } from "./hello";
-import { NameEditComponent } from "./nameEdit";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-export const App = () => {
-  const [name, setName] = React.useState("sathia");
+import {MovieDetail} from './moviedetails'
 
-  const setUsernameState = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
+interface State{
+  items: any,
+  isLoaded:boolean
+}
+interface Props{
+}
 
-  const getUsernameState = (event: React.MouseEvent<HTMLInputElement>) => {
-   setName('murthi');
-  };
 
-  return (
-    <>
-      <HelloComponent userName={name} />
-      <NameEditComponent userName={name} onChange={setUsernameState} onClick={getUsernameState} />
-    </>
-  );
+export class App extends React.Component<Props, State> {
+
+  constructor(props){
+    super(props);
+    this.state = { items: [], isLoaded:false };
+  }
+
+    public componentDidMount() {
+      console.log('i am called');
+    fetch("http://www.omdbapi.com/?apikey=8d13ddf&type=movie&s=bad")
+    .then(resp => resp)
+    .then(resp => resp.json())
+    .then(response => {
+        if (response.Response === 'False') {
+            
+        }
+        else {
+          console.log('data',response.Search)
+          this.setState({items:response.Search});
+        }}),
+       
+        (error) => {
+          this.setState({
+            isLoaded: false
+          });
+        }
+  
+  }
+  public render() {
+    return (
+      <div className="row">
+        <h2>Movies</h2>
+        <table className="table">
+          <tbody>
+            {
+              this.state.items.map((item) =>
+                <MovieDetail title={item.Title} year={item.Year} imdbID={item.imdbID} type={item.Type} poster={item.Poster}/>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
